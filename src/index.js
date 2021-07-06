5354f555dce6a6ea3bed87a572d5d4b2f68ea6d7
github username <github email address>
2021-07-06 16:11:44 -0400

    commit tree 46100fad91637b5aedcd5586adab5917c4f737d4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-const init = () => {
-  
+const init = () => { //write code here//
+    const inputForm = document.querySelector('form');
+    
+    inputForm.addEventListener('submit', (event)=> {
+        event.preventDefault();
+        console.log(event);
+        
+    })
+    
 }
+
 
 document.addEventListener('DOMContentLoaded', init);
